882f0813b2f8deab4e5f8704bbb752b90f7ab63d
cryptonaut <nickrathman@gmail.com>
2019-08-04 00:49:51 -0700

    custom redirect on logout

--- frontend/js/logout.js
+++ frontend/js/logout.js
@@ -3,6 +3,12 @@
 
   global.logout = function() {
     global.cookieDelete("commentoOwnerToken");
+	var urlParams = new URLSearchParams(window.location.search);
+	var redirect = urlParams.get('redirect');
+	if(redirect){
+		document.location = redirect;
+		return;
+	}
     document.location = global.origin + "/login";
   }
 
