7830a002857076c957a686c1427cc3c3381db341
cryptonaut <nickrathman@gmail.com>
2019-08-06 00:37:53 -0700

    update getenv

--- api/comment_new.go
+++ api/comment_new.go
@@ -162,13 +162,13 @@ func commentNewHandler(w http.ResponseWriter, r *http.Request) {
 		check_data.Route = path
 		check_data.PermKey = "canComment"
 		check_json, err := json.Marshal(check_data)
-		secret_bytes, err := hex.DecodeString(os.GetEnv("PARENT_APP_API_SECRET"))
+		secret_bytes, err := hex.DecodeString(os.Getenv("PARENT_APP_API_SECRET"))
 		h := hmac.New(sha256.New, secret_bytes)
 		h.Write(check_json)
 		SignatureBytes := h.Sum(nil)
 		hmac_string := string(SignatureBytes)
 
-		resp, err := http.PostForm(os.GetEnv("PARENT_APP_URL") + "/api/v1/permissions/check",
+		resp, err := http.PostForm(os.Getenv("PARENT_APP_URL") + "/api/v1/permissions/check",
 			url.Values{"requester": {"commento"}, "email": {c.Email}, "route": {path}, "permKey": {"canComment"}, "hmac": {hmac_string}})
 		
 		if err != nil {
